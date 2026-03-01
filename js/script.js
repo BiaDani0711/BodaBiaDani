5e21ab16505ab549cb122cd0b054f43fd10d9c38
BiaDani0711 <biadani1309@gmail.com>
2026-03-01 12:58:38 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-document.getElementById("timer").innerHTML =
-days + " días " + hours + "h " + minutes + "m " + seconds + "s";
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
 
 }, 1000);
